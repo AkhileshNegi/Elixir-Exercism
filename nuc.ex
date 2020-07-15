e4f62d0e5c36609f51f677ed073ac820a5d30272
AkhileshNegi <akhileshnegi.an3@gmail.com>
2020-07-15 16:27:11 +0530

    working but with line break

--- nuc.ex
+++ nuc.ex
@@ -1,14 +1,12 @@
 
 defmodule DNA do
-    def to_rna(dna) do
-		rna = %{
-            "G"=>"C",
-            "C"=>"G",
-            "T"=>"A",
-            "A"=>"U",
-            }
-    dna|>Enum.each(fn ch -> 
-      IO.puts"#{rna["ch"]}"
+    def to_rna(dna) do	
+    dna|>Enum.each(fn ch ->cond do
+      [ch] == 'G' ->IO.puts("C") 
+      [ch] == 'C' ->IO.puts("G") 
+      [ch] == 'T' ->IO.puts("A") 
+      [ch] == 'A' ->IO.puts("U") 
+      end
        end)
     end
 end
--- word_count.ex
+++ word_count.ex
@@ -1,7 +1,9 @@
 defmodule Word do
   @spec count(String.t()) :: map
   def count(sentence) do
+  	  map = %{}
   	String.downcase(sentence) |> String.split(" ") 
-  	|> Enum.each(fn(s) -> IO.puts(s) end)
+  	|> Enum.each(fn(s) -> new_map = Dict.put_new(map, s, "1") end)
+IO.puts(new_map[:new_val])
   	  end
 end
