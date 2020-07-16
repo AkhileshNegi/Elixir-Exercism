ea016084675a65c1eefde13e5686bcc27546783e
AkhileshNegi <akhileshnegi.an3@gmail.com>
2020-07-16 14:43:31 +0530

    Bob can differentiate between Yell and Yell n Question

--- Bob.ex
+++ Bob.ex
@@ -7,13 +7,19 @@ defmodule Bob do
 			"Nothing" => "Fine. Be that way",
 			"Anything" => "Whatever"
 			}
-		bol = input =~ ~r(^[^a-z]*$)
-		if bol===true do
-			expression = "Yell"
-		IO.puts"#{reaction["#{expression}"]}"
+		iscapital = input =~ ~r(^[^a-z]*$)
+		if iscapital === true do
+			lastchar = String.ends_with?("#{input}", "?")
+			if lastchar === true do
+				expression = "Yell and Question"
+				IO.puts"#{reaction["#{expression}"]}"
+			else
+				expression = "Yell"
+				IO.puts"#{reaction["#{expression}"]}"
+			end
 		else
 			expression = "Nothing"
-		IO.puts"#{reaction["#{expression}"]}"
+			IO.puts"#{reaction["#{expression}"]}"
 		end
 	end
 end
